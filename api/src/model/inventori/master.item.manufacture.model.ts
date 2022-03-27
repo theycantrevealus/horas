1a9a55fd3fbf67c09caf9e59f0fd2b8c95ac4b9b
Hendry Tanaka <theycantrevealus@gmail.com>
2022-03-27 18:28:58 +0700

    Inventori Model Init
    1. Master Item
    2. Master Item Alias
    3. Master Item Manufacture
    4. Master Item Category
    5. Master Item Manufacture Join
    6. Master Item Category Join

---
+++ api/src/model/inventori/master.item.manufacture.model.ts
@@ -0,0 +1,36 @@
+import {
+    Entity,
+    Column,
+    PrimaryColumn,
+    Generated,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    ManyToOne,
+} from 'typeorm'
+
+@Entity({ name: 'master_item_manufacture' })
+export class MasterItemManufactureModel {
+    @PrimaryColumn()
+    @Generated('uuid')
+    @Column({ nullable: false, type: 'uuid', primary: true })
+    uid: string
+
+    @Column({ nullable: false, unique: true, type: 'character varying' })
+    code: string
+
+    @Column({ nullable: false, type: 'character varying' })
+    name: string
+
+    @Column({ type: 'text' })
+    remark: string
+
+    @CreateDateColumn({ nullable: false, type: 'timestamp without time zone' })
+    created_at: Date
+
+    @UpdateDateColumn({ nullable: false, type: 'timestamp without time zone' })
+    updated_at: Date
+
+    @DeleteDateColumn({ nullable: true, type: 'timestamp without time zone' })
+    deleted_at: Date
+}
