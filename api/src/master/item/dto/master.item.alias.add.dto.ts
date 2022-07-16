c3c6961c7477004b912abda3f11e8b5f39e2a08f
Hendry Tanaka <theycantrevealus@gmail.com>
2022-07-17 01:11:20 +0700

    Master Item Update Feature
    1. CRUD Master Item
    2. CRUD Master Item Aliases

---
+++ api/src/master/item/dto/master.item.alias.add.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsString, IsNumber } from 'class-validator'
+
+export class MasterItemAliasAddDTO {
+  @ApiProperty({
+    example: 'Paracetamol'
+  })
+  @IsString()
+  name: string
+
+  @ApiProperty({
+    example: 'Item UID'
+  })
+  @IsString()
+  item: any
+}
+
+export class MasterItemAliasAddDTOResponse {
+  @ApiProperty({ example: 201 })
+  @IsNumber()
+  status: number
+
+  @ApiProperty({ example: 'Item Alias Registered Successfully' })
+  @IsString()
+  message: string
+
+  returning: any
+}
