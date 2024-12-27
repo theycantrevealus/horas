0fb6b47f958f6816b332cec98a301fd2d5f0744a
Hendry Tanaka <theycantrevealus@gmail.com>
2024-12-27 15:33:12 +0700

    🔧 fix: remove lov middleware router

--- frontend/v2/src/modules/master/lov/Module.vue
+++ frontend/v2/src/modules/master/lov/Module.vue
@@ -6,11 +6,5 @@ import { toggle_confirm } from '@/utils/core/page.editing'
 
 export default {
   name: 'LOV',
-  beforeRouteLeave(to, from, next) {
-    // toggle_confirm(this)
-    // if(confirmZ) {
-    //   next()
-    // }
-  }
 }
 </script>
