8a9b5b46d83a02e4fdfa729dc0a1199d645eeaa9
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-05-18 23:44:01 +0700

    🔧 fix: print component - add information for all paper size

--- frontend/v2/src/components/print/Option.vue
+++ frontend/v2/src/components/print/Option.vue
@@ -3,6 +3,7 @@
     v-model:visible="ui.drawer.visibility"
     header="Print Options"
     position="right"
+    class="w-3"
     @update:visible="updateVisibility"
   >
     <div class="p-3">
@@ -14,8 +15,23 @@
           optionLabel="name"
           placeholder="Select a paper size"
           class="w-full md:w-56"
+          @change="updateInformation"
         /><label for="paper_size">Paper Size</label>
       </FloatLabel>
+
+      <Message
+        class="my-4"
+        severity="info"
+        icon="pi pi-info-circle"
+        :pt="{ icon: { class: 'message-icon-top' } }"
+      >
+        <strong>Tractor feed margin:</strong> usually 1/4 inch (6-8 mm) to 1/2 inch (12-15 mm) on
+        each side.
+        <br />
+        <strong>Holes:</strong> typically 1/4 inch (6 mm) in diameter, spaced 1/2 inch (12 mm) apart
+        <Divider v-if="ui.hint !== ''" />
+        {{ ui.hint }}
+      </Message>
 
       <div class="flex flex-row-reverse p-6">
         <Button
@@ -34,6 +50,7 @@ export default defineComponent({
   data() {
     return {
       ui: {
+        hint: '',
         drawer: {
           visibility: false,
           selectedPaperSize: {
@@ -43,9 +60,24 @@ export default defineComponent({
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
           } as any,
           paperSizes: [
-            { code: 'con_small', name: 'Continuous Small', orientation: 'landscape' },
-            { code: 'con_medium', name: 'Continuous Medium', orientation: 'landscape' },
-            { code: 'con_long', name: 'Continuous Long', orientation: 'portrait' },
+            {
+              code: 'con_small',
+              name: 'Continuous Short (241mm ⨉ 93mm)',
+              orientation: 'landscape',
+              description: 'Continuous form 241mm ⨉ 93mm',
+            },
+            {
+              code: 'con_medium',
+              name: 'Continuous Medium (241mm ⨉ 140mm)',
+              orientation: 'landscape',
+              description: 'Continuous form 241mm ⨉ 140mm',
+            },
+            {
+              code: 'con_long',
+              name: 'Continuous Long (241mm ⨉ 280mm)',
+              orientation: 'portrait',
+              description: 'Continuous form 241mm ⨉ 280mm',
+            },
           ],
         },
       },
@@ -64,6 +96,10 @@ export default defineComponent({
     },
   },
   methods: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    updateInformation(event: any) {
+      this.ui.hint = event.value.description
+    },
     updateVisibility() {
       this.$emit('update-visibility', this.ui.drawer.visibility)
     },
